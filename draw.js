902d112fa0d6bc0a18284f69c5df37f5bb5eae65
swooboo <swooboo@mail.ru>
2019-09-22 15:18:55 +0300

    Revert back to single pixel

--- draw.js
+++ draw.js
@@ -1,18 +1,22 @@
 window.onload = function(){
 	var canvas = document.getElementById('myCanvas');
 	var context = canvas.getContext('2d');
+
+	var mirror = Array(canvas.width).fill(Array(canvas.height).fill(0));	// Counters for points
 }
 
 // Drawing all the points using an existing generator
 function draw_points(init=0, last=0, drawer_g=function*(){return [0,0];}, canvas, context) {
-	canvas_data = context.getImageData(0, 0, canvas.width, canvas.height);
-
+	var image_data = context.createImageData(1,1); 
+	var d  = image_data.data;
+	d[0] = 0;
+	d[1] = 0;
+	d[2] = 0;
+	d[3] = 255; // Create and color a pixel
 	gener = drawer_g(init, last); // Get the generator needed
 	for (let point of gener) {
 		[x,y] = point;
-		[x,y] = [x%canvas.width,y%canvas.width]	// Don't draw outside of canvas
-		context.beginPath();
-		context.arc(x, y, 1, 0, Math.PI * 2, true);
-		context.stroke(); //Draw the thing
+		[x,y] = [x%canvas.width,y%canvas.height]	// Don't draw outside of canvas
+		context.putImageData(image_data, x, y); // Draw the pixel
 	}
 }
